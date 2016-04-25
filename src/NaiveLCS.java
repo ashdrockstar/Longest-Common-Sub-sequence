a5f4a609b2af3a43400f52dc0d537dfabd8dee21
Aishwary Pramanik <aishwarypramanik@gmail.com>
2016-04-25 15:21:38 -0400

    Completed Naive Without Comments

---
+++ src/NaiveLCS.java
@@ -0,0 +1,26 @@
+
+public class NaiveLCS {
+	public StringBuffer findLCS(StringBuffer A,StringBuffer B)
+	{
+		int i,j,k;
+		i=0;
+		j=0;
+		StringBuffer sub=new StringBuffer();
+		while(j<B.length())
+		{
+			k=i;
+			while(k<A.length())
+			{
+				if(A.charAt(k)==B.charAt(j))
+				{
+					sub.append(A.charAt(k));
+					i++;
+					break;
+				}
+				k++;
+			}
+			j++;
+		}
+		return sub;
+	}
+}
