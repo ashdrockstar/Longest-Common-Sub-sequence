cc78b73822bc17b0ca92f339e8252db4140afc6a
Aishwary Pramanik <aishwarypramanik@gmail.com>
2016-05-03 12:59:37 -0400

    Considered A-B and B-A comparisons.

--- src/NaiveLCS.java
+++ src/NaiveLCS.java
@@ -3,24 +3,47 @@ public class NaiveLCS {
 	public StringBuffer findLCS(StringBuffer A,StringBuffer B)
 	{
 		int i,j,k;
+		StringBuffer sub1=new StringBuffer();
+		StringBuffer sub2=new StringBuffer();
 		i=0;
 		j=0;
-		StringBuffer sub=new StringBuffer();
-		while(j<B.length())
+		while(i<A.length())
 		{
-			k=i;
-			while(k<A.length())
+			k=j;
+			while(k<B.length())
 			{
-				if(A.charAt(k)==B.charAt(j))
+				if(A.charAt(i)==B.charAt(k))
 				{
-					sub.append(A.charAt(k));
-					i++;
+					sub1.append(A.charAt(i));
+					j=k+1;
 					break;
 				}
 				k++;
 			}
-			j++;
+			i++;
 		}
-		return sub;
+
+		i=0;
+		j=0;
+		while(i<B.length())
+		{
+			k=j;
+			while(k<A.length())
+			{
+				if(B.charAt(i)==A.charAt(k))
+				{
+					sub2.append(B.charAt(i));
+					j=k+1;
+					break;
+				}
+				k++;
+			}
+			i++;
+		}
+		
+		if(sub1.length()>sub2.length())
+			return sub1;
+		else
+			return sub2;
 	}
 }
