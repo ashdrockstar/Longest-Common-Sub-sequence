c123343552af7a4e5a7b44750f9d630a45d6b1ab
Aishwary Pramanik <aishwarypramanik@gmail.com>
2016-05-02 23:58:14 -0400

    Stage 2

--- src/ExecuteAlgo.java
+++ src/ExecuteAlgo.java
@@ -27,6 +27,9 @@ public class ExecuteAlgo {
 		// Finding LCS using Dynamic programming version of the algorithm
 		NaiveLCS naiveLCS=new NaiveLCS();
 
+		// Finding LCS using Dynamic programming version of the algorithm
+		RecursionMemo recLCS=new RecursionMemo();
+
 		// To randomly generate binary numbers or alphabets
 		Random choice=new Random();
 		// To store input1
@@ -41,43 +44,46 @@ public class ExecuteAlgo {
 		long cpuTime=0;
 		long startTime;
 		long endTime;
+
 		//FileWriter fileWriter=new FileWriter(new File("DynamicLCSLog.csv"));
-		FileWriter fileWriter=new FileWriter(new File("NaiveLCSLog.csv"));
-		fileWriter.write("CPU Time(ms),Input Length,LCS Length\n");
-		System.out.println("|CPU Time(ms)|\t|Input Length|\t|LCS Length|");
+		//FileWriter fileWriter=new FileWriter(new File("NaiveLCSLog.csv"));
+		//FileWriter fileWriter=new FileWriter(new File("RecMemoLCSLog.csv"));
+		//		fileWriter.write("CPU Time(ms),Input Length,LCS Length\n");
+		//		System.out.println("|CPU Time(ms)|\t|Input Length|\t|LCS Length|");
 		// Loop which ends when execution time exceeds 10 seconds
-		while(cpuTime<=10000)
+		//		while(cpuTime<=10000)
+		//		{
+		S1.delete(0, S1.length());
+		S2.delete(0, S2.length());
+		if(choice.nextInt(2)==0)
 		{
-			S1.delete(0, S1.length());
-			S2.delete(0, S2.length());
-			if(choice.nextInt(2)==0)
+			for(int i=0;i<ctr;i++)
 			{
-				for(int i=0;i<ctr;i++)
-				{
-					S1.append(choice.nextInt(2));
-					S2.append(choice.nextInt(2));
-				}
+				S1.append(choice.nextInt(2));
+				S2.append(choice.nextInt(2));
 			}
-			else
-			{
-				for(int i = 0; i < ctr; i++)
-				{
-					S1 = S1.append((char)('A' + choice.nextInt(26)));
-				}
-				for(int i = 0; i < ctr; i++)
-				{
-					S2 = S2.append((char)('A' + choice.nextInt(26)));
-				}
-			}
-			startTime=System.currentTimeMillis();
-			//sub=dynamicLCS.findLCS(S1,S2);
-			sub=naiveLCS.findLCS(S1,S2);
-			endTime=System.currentTimeMillis();
-			cpuTime=endTime-startTime;
-			System.out.println("|"+cpuTime+"|\t\t|"+ctr+"|\t\t|"+sub.length()+"|");
-			fileWriter.write(cpuTime+","+ctr+","+sub.length()+"\n");
-			ctr++;
 		}
+		else
+		{
+			for(int i = 0; i < ctr; i++)
+			{
+				S1 = S1.append((char)('A' + choice.nextInt(26)));
+			}
+			for(int i = 0; i < ctr; i++)
+			{
+				S2 = S2.append((char)('A' + choice.nextInt(26)));
+			}
+		}
+		startTime=System.currentTimeMillis();
+		//sub=dynamicLCS.findLCS(S1,S2);
+		sub=naiveLCS.findLCS(new StringBuffer("BBACBCA"),new StringBuffer("BABCCBAB"));
+		//		sub=recLCS.findLCS(new StringBuffer("BABCCBAB"),new StringBuffer("BBACBCA"));
+		//			endTime=System.currentTimeMillis();
+		//			cpuTime=endTime-startTime;
+		//			System.out.println("|"+cpuTime+"|\t\t|"+ctr+"|\t\t|"+sub.length()+"|");
+		//			fileWriter.write(cpuTime+","+ctr+","+sub.length()+"\n");
+		//			ctr++;
+		//		}
 		System.out.println("----------------TERMINATION-------------");
 		System.out.println("\nS1 Length:"+S1.length());
 		System.out.println("S1:\n"+S1);
--- src/NaiveLCS.java
+++ src/NaiveLCS.java
@@ -13,7 +13,6 @@ public class NaiveLCS {
 			{
 				if(A.charAt(k)==B.charAt(j))
 				{
-					
 					sub.append(A.charAt(k));
 					i++;
 					break;
